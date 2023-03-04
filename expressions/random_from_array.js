e4abe1010085e90e77eabeac26f4d83ea9af65ee
Johannes <hello@thejoltjoker.com>
2023-03-04 12:22:03 +0100

    add random emitter type

---
+++ expressions/random_from_array.js
@@ -0,0 +1,6 @@
+// Wiggle the emitter type
+// Posterize time to only happen once per second
+posterizeTime(1);
+var emitters = [1,2,3,4,5,6];
+var emitter = emitters[Math.floor(Math.random()*emitters.length)];
+emitter;
